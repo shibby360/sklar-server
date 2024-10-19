1a00b7f434b9cdfd88c0953e15fcce8ed8a827b0
Shivank Chhaya <shivank.chhaya@gmail.com>
2024-10-18 22:44:08 -0700

    changed render_template to "home"

--- api/main.py
+++ api/main.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return 'home'
 
 @app.route('/about')
 def about():
