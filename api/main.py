03a4ea8e28fc5df9793e7b1ef23e05c795c4effe
Shivank Chhaya <shivank.chhaya@gmail.com>
2024-10-20 17:41:47 +0000

    added soph-honors route

--- api/main.py
+++ api/main.py
@@ -1,7 +1,16 @@
 from flask import Flask
+import requests
+from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return 'home'
+
+@app.route('/soph-honors')
+def soph_honors():
+    r = requests.get('https://sklarnation.com/sophomore-honors-english').text
+    soup = BeautifulSoup(r, 'html.parser')
+    maindiv = soup.find_all('div', class_='entry-content')[0]
+    return str(maindiv)
