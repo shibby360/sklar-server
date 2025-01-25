f9c2ede16e8e0a35f948ffb6e5e28ca714247ee6
Shivank Chhaya <shivank.chhaya@gmail.com>
2025-01-25 04:41:29 +0000

    added soph-honors-today route

--- api/main.py
+++ api/main.py
@@ -1,8 +1,18 @@
 from flask import Flask, jsonify, make_response, request
 import requests
 from bs4 import BeautifulSoup
+from datetime import datetime
+import pytz
 
 app = Flask(__name__)
+def convert_date_to_days(stringdate):
+    months = {'January':0,'February':31,'March':28,'April':31,'May':30} # days in month before
+    end = 0
+    for i in months:
+        end += months[i]
+        if stringdate.split(' ')[0] == i:
+            break
+    return end + int(stringdate.split(' ')[1])
 
 @app.route('/')
 def home():
@@ -18,6 +28,27 @@ def soph_honors():
         maindiv = soup.find_all('div', class_='entry-content')[0]
         return _corsify_actual_response(make_response(str(maindiv)))
 
+@app.route('/soph-honors-today')
+def soph_honors_today():
+    today = datetime.now(pytz.timezone("America/Los_Angeles")).strftime('%B %d')
+    dayname = datetime.now(pytz.timezone("America/Los_Angeles")).strftime('%A')
+    r = requests.get('https://sklarnation.com/sophomore-honors-english').text
+    soup = BeautifulSoup(r, 'html.parser')
+    maindiv = soup.find_all('div', class_='entry-content')[0]
+    foundweek = False
+    for i in maindiv.findAll('p'):
+        if i.text.startswith('Week'):
+            if foundweek: break
+            datestring = i.text.split('— ')[1].split(',')[0]
+            dates = datestring.split('-')
+            if not dates[1].isalpha():
+                dates[1] = dates[0].split(' ')[0] + ' ' + dates[1]
+            if convert_date_to_days(dates[0]) <= convert_date_to_days(today) <= convert_date_to_days(dates[1]):
+                foundweek = True
+        elif foundweek:
+            if i.text.startswith(dayname):
+                return i.text.split('— ')[1]
+    return ''
 
 def _build_cors_preflight_response():
     response = make_response()
@@ -29,3 +60,6 @@ def _build_cors_preflight_response():
 def _corsify_actual_response(response):
     response.headers.add("Access-Control-Allow-Origin", "*")
     return response
+
+if __name__ == '__main__':
+    app.run()
